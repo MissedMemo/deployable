87ad34283b22edca895072e4bb35e10066b626e1
Andrew Lloyd <MissedMemo@GMail.com>
2016-03-27 15:57:48 -0700

    basic node express implementation to serve static index.html

---
+++ README.md
@@ -0,0 +1,46 @@
+[![Stories in Ready](https://badge.waffle.io/Flatten-Threat/Spree.png?label=ready&title=Ready)](https://waffle.io/Flatten-Threat/Spree)
+# Spree
+
+Product Owner: Jenna Vuong
+
+Lead Developer: Andrew Lloyd
+
+Scrum Master: Tritia Timmins
+
+Instructions:
+
+    npm install
+    
+to start with webpack-dev-server (port 8080):
+
+    npm start
+
+to start with express server (PORT optional, defaults to 3000):
+
+    mongod (start in a separate terminal)
+    
+    PORT=4568 NODE_ENV=production npm start
+
+to view:
+
+    browse to localhost + port (e.g. localhost:8080 etc.)
+
+
+Tech. Stack:
+
+    React
+    React Router
+    Webpack
+    Web Fonts
+    Express
+    MongoDB
+    Axios
+    (planned) - Heroku
+    (planned) - React Native
+    (planned) - Mocha/Chai
+    (probably) - Bootstrap
+    (probably) - Less
+    (maybe) - Google maps & mobile device geo-loc
+    (maybe) - server-side rendering
+    (maybe) - Redux, Redux Router
+    (maybe) - mySQL, D3
---
+++ server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var path = require('path');
+/*
+var compression = require('compression');
+var mongoose = require('mongoose');
+var demoData = require( './models/demo-data' );
+*/
+var app = express();
+//app.use( compression() ); // must come first!
+
+
+// serve static files like index.html, css etc.
+app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+
+var PORT = process.env.PORT || 3000;
+
+
+var server = app.listen(PORT, function() {
+
+  console.log('\nProduction Express server running at localhost:' + PORT);
+
+  /*
+  var dbURI = 'mongodb://localhost/adventureUS';
+
+  mongoose.connect( dbURI, function(error) {
+    if(error) {
+      console.log( '\n****** ERROR! ****** ERROR! ****** ERROR! ******' );
+      console.log( 'Unable to connect to: ' + dbURI + ' Is mongod running?' );
+      console.log( '\nShutting down all servers...\n' );
+      server.close();
+    }
+  });
+
+  mongoose.connection.on( 'connected', function () {
+    console.log( 'successful db connection to: ' + dbURI + '\n' );
+    demoData.initDatabase(); // clear database, and seed with demo data
+  });
+  */
+
+});
+
+
+
+/***********************************************************
+  move this, and other API handlers, into their own file...
+***********************************************************/
+/*
+var Activity = require( './models/activity' );
+
+app.get( '/api/activities', function( req, res ) {
+
+  Activity.find( {}, function(error, data) {
+
+    if(error) {
+      res.json(error);
+    }
+    else if( data === null ) {
+      res.json('Empty data')
+    }
+    else {
+      res.json(data);
+    }      
+  });
+
+});
+*/
